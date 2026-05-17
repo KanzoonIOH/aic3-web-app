9ea9d5da1d5a47756260a4d15c1729b33a9d3afd
kanzoonrekza <kanzoonrkz@gmail.com>
2026-05-17 22:00:32 +0700

    feat: use env for baseUrl

--- src/api/client.ts
+++ src/api/client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "@/stores/auth";
 
 export const client = axios.create({
-    baseURL: "http://localhost:6767/api",
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
